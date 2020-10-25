fc91e1872009f8c3b250f4048c0714a1783389a1
Emily Melendez <emi.mci.95@gmail.com>
2020-10-25 16:15:35 -0600

    added script postgre and procedures sql server

--- ScriptDataBaseSQL/Script_Create_Procedures.sql
+++ ScriptDataBaseSQL/Script_Create_Procedures.sql
@@ -45,11 +45,131 @@ SELECT
 	[LAST_UPDATE]	
 FROM [dbo].[PHONE_CLIENT]
 
-exec get_all_phone_clients
-select * from [dbo].[PHONE_CLIENT]
+CREATE PROCEDURE get_all_credit_cards
+AS
+SELECT 
+    CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('CARD_NUMBER', CARD_NUMBER)) as CARD_NUMBER,	
+	CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('CVV', CVV)) as CVV,
+	[ID_CLIENT],
+	CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('EXPIRATION', EXPIRATION)) as EXPIRATION,	
+	[LAST_UPDATE]	
+FROM [dbo].[CREDIT_CARD]
+
+CREATE PROCEDURE get_all_email_clients
+AS
+SELECT 
+	[ID],
+	[ID_CLIENT],
+    CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('EMAIL', EMAIL)) as EMAIL,	
+	[LAST_UPDATE]	
+FROM [dbo].[EMAIL_CLIENT]
+
+
+exec get_all_email_clients
+select * from [dbo].[CREDIT_CARD]
 
 
 SELECT * FROM [dbo].[EMAIL_CLIENT]
+
+
+
+
+
+
+---PROCEDURE WITH PARAMETER
+CREATE PROCEDURE get_all_clients_by_date
+@init_date date,
+@final_date date
+AS
+SELECT 
+	[ID],
+	[NAME],
+	[LAST_NAME],
+	[AGE],
+	[LAST_UPDATE]
+FROM [dbo].[CLIENT] 
+WHERE [LAST_UPDATE] >= @init_date AND
+	  [LAST_UPDATE] <= @final_date
+
+CREATE PROCEDURE get_all_sales_by_date
+@init_date date,
+@final_date date
+AS
+SELECT 
+	[ID],
+	[ID_CLIENT],
+	[DESCRIPTION],
+	[TOTAL],
+	[LAST_UPDATE]
+FROM [dbo].[SALE]
+WHERE [LAST_UPDATE] >= @init_date AND
+	  [LAST_UPDATE] <= @final_date
+
+CREATE PROCEDURE delete_all_clients
+AS
+DELETE FROM [dbo].[CLIENT]
+
+CREATE PROCEDURE delete_all_sales
+AS
+DELETE FROM [dbo].[SALE]
+
+CREATE PROCEDURE get_all_address_clients_by_date
+@init_date date,
+@final_date date
+AS
+SELECT 
+	[ID],	
+    [ID_CLIENT],
+    CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('ADDRESS', ADDRESS)) as ADDRESS,
+	[ADDRESS_NAME],
+	[LAST_UPDATE]	
+FROM [dbo].[ADDRESS_CLIENT]
+WHERE [LAST_UPDATE] >= @init_date AND
+	  [LAST_UPDATE] <= @final_date
+
+CREATE PROCEDURE get_all_phone_clients_by_date
+@init_date date,
+@final_date date
+AS
+SELECT 
+	[ID],	
+    [ID_CLIENT],
+    CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('NUMBER', NUMBER)) as NUMBER,	
+	[LAST_UPDATE]	
+FROM [dbo].[PHONE_CLIENT]
+WHERE [LAST_UPDATE] >= @init_date AND
+	  [LAST_UPDATE] <= @final_date
+
+CREATE PROCEDURE get_all_credit_cards_by_date
+@init_date date,
+@final_date date
+AS
+SELECT 
+    CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('CARD_NUMBER', CARD_NUMBER)) as CARD_NUMBER,	
+	CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('CVV', CVV)) as CVV,
+	[ID_CLIENT],
+	CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('EXPIRATION', EXPIRATION)) as EXPIRATION,	
+	[LAST_UPDATE]	
+FROM [dbo].[CREDIT_CARD]
+WHERE [LAST_UPDATE] >= @init_date AND
+	  [LAST_UPDATE] <= @final_date
+
+CREATE PROCEDURE get_all_email_clients_by_clients
+@init_date date,
+@final_date date
+AS
+SELECT 
+	[ID],
+	[ID_CLIENT],
+    CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('EMAIL', EMAIL)) as EMAIL,	
+	[LAST_UPDATE]	
+FROM [dbo].[EMAIL_CLIENT]
+WHERE [LAST_UPDATE] >= @init_date AND
+	  [LAST_UPDATE] <= @final_date
+
+
+
+
 
 
 
